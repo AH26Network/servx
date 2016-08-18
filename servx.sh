5164d214276c617d5afd5ee6929825185b1f84ca
AH26 <contact@ah26.ovh>
2016-08-18 17:44:35 +0200

    Update servx.sh

--- servx.sh
+++ servx.sh
@@ -9,9 +9,11 @@ SERVX_VERSION="1"
 OS=$(uname -s)
 VER=$(uname -r)
 ARCH=$(uname -m)
+DATE=$(date)
 
 # - Welcome msg - 
 echo "#######################################"
 echo "Welcome on Servx Automated Installation"
 echo "#######################################"
-echo "Detected : $OS  $VER  $ARCH"
+echo "OS Detected : $OS  $VER  $ARCH"
+echo "Current date : $DATE"
