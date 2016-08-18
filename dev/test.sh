179d78a6876eb4553b9062ca6959b8f4e6512baf
AH26 <contact@ah26.ovh>
2016-08-19 01:36:59 +0200

    Update test.sh

--- dev/test.sh
+++ dev/test.sh
@@ -3,3 +3,5 @@ GAMETOINSTALL="rust"
 SRVID="1"
 password="1234"
 echo sshpass -p $password ssh $GAMETOINSTALL-$SRVID@localhost mkdir test
+sleep 5
+sshpass -p $password ssh $GAMETOINSTALL-$SRVID@localhost mkdir test
