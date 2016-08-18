9f1a70bb16a8872dbeded0f92be25a0d9c03c9a2
OC018 <www.sebastienroulph@hotmail.fr>
2016-08-19 00:38:56 +0200

    Update Gmod.sh

--- dev/Gmod.sh
+++ dev/Gmod.sh
@@ -3,8 +3,9 @@ echo "Downloading SteamCMD ..."
 wget -O /home/$GAMETOINSTAL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 cd /home/$GAMETOINSTAL-$SRVID/
 tar -xvzf steamcmd_linux.tar.gz
-echo "Steam Downloading sucess"
+echo "Steam Downloading success"
 chmod +x Steamcmd.sh
-./steamcmd.sh
-
+echo "Downloading Gmod"
+./steamcmd.sh +login anonymous +app_update 4020 validate +quit
+echo "Gmod Downloading success"
 fi
