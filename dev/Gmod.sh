20906cf3414253f115ad8ae40f8791bde2d260da
OC018 <www.sebastienroulph@hotmail.fr>
2016-08-19 00:35:02 +0200

    Update Gmod.sh

--- dev/Gmod.sh
+++ dev/Gmod.sh
@@ -1,7 +1,10 @@
 if [[ "$GAMETOINSTALL" = "gmod" ]] ; then
 echo "Downloading SteamCMD ..."
-fi
-echo "Downlaoding SteamCMD ..." 
 wget -O /home/$GAMETOINSTAL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+cd /home/$GAMETOINSTAL-$SRVID/
 tar -xvzf steamcmd_linux.tar.gz
-echo 
+echo "Steam Downloading sucess"
+chmod +x Steamcmd.sh
+./steamcmd.sh
+
+fi
