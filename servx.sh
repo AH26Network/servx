22c60e2a2bad8bc457d3db876d7588ab32e30d51
AH26 <contact@ah26.ovh>
2016-08-18 21:31:41 +0200

    Update servx.sh

--- servx.sh
+++ servx.sh
@@ -53,6 +53,25 @@ apt-get upgrade -y
 sleep 1
 apt-get install screen -y
 
+# - Create User - 
+if [ $(id -u) -eq 0 ]; then
+	#read -p "Enter username : " username
+	$username = $GAMETOINSTALL_$SRVID
+	read  -p "Enter password for $GAMETOINSTALL Server $SRVID : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p $pass $username
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system"
+	exit 2
+fi
+
 # - Install - 
 # - Minecraft - 
 if [[ "$GAMETOINSTALL" = "minecraft" ]] ; then
