a22e3cbf901bfffcda4e6c7a99c41cf7a5a3f366
AH26 <contact@ah26.ovh>
2016-08-18 23:58:52 +0200

    Update servx.sh

--- servx.sh
+++ servx.sh
@@ -86,9 +86,9 @@ read MCVS
     elif [[ "$MCVS" == "1.7.10" ]]; then
         echo "Installing Minecraft Server in $MCVS"
         echo "Downloading Spigot in $MCVS"
-	wget -O /home/$GAMETOINSTALL-$SRVID/ http://www.thetueurcity.com/download/spigot1.7.10.jar
+	wget -O /home/$GAMETOINSTALL-$SRVID/spigot1.7.10.jar http://www.thetueurcity.com/download/spigot1.7.10.jar
 	chmod +x /home/$GAMETOINSTALL-$SRVID/spigot1.7.10.jar
-	echo screen -h 1024 -dmS $GAMETOINSTALL-$SRVID java -Xms512M -Xmx1024M -jar -XX:ParallelGCThreads=1 craftbukkit.jar >> /home/$GAMETOINSTALL-$SRVID/start.sh
+	echo screen -h 1024 -dmS $GAMETOINSTALL-$SRVID java -Xms512M -Xmx1024M -jar -XX:ParallelGCThreads=1 spigot1.7.10.jar >> /home/$GAMETOINSTALL-$SRVID/start.sh
 	chmod +x /home/$GAMETOINSTALL-$SRVID/start.sh
     else
         echo "No version found"
