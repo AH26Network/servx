2daa4d8ab811f6ee036683ad50bb86a581dfbc58
OC018 <www.sebastienroulph@hotmail.fr>
2016-08-19 01:09:38 +0200

    Update Gmod.sh

--- dev/Gmod.sh
+++ dev/Gmod.sh
@@ -1,20 +1,21 @@
 if [[ "$GAMETOINSTALL" = "gmod" ]] ; then
 echo "Downloading SteamCMD ..."
-wget -O /home/$GAMETOINSTAL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-cd /home/$GAMETOINSTAL-$SRVID/
-tar -xvzf steamcmd_linux.tar.gz
+    wget -O /home/$GAMETOINSTAL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+    cd /home/$GAMETOINSTAL-$SRVID/
+    tar -xvzf steamcmd_linux.tar.gz
 echo "Steam Downloading success"
-chmod +x Steamcmd.sh
+    chmod +x Steamcmd.sh
 echo "Downloading Gmod"
-./steamcmd.sh +login anonymous +app_update 4020 validate +quit
+    ./steamcmd.sh +login anonymous +app_update 4020 validate +quit
 echo "Gmod Downloading success"
-read -p "Number of Slots ?" GMOD_SLOTS
+  read -p "Number of Slots ?" GMOD_SLOTS
 echo -e "${GREEN}Game Mode suported"
 echo -e "Sandbox;DarkRP;Muder;PropHunt;JailBreak${NC}"
-read -p "What Game Mode" GMOD_GAMEMODE
+  read -p "What Game Mode" GMOD_GAMEMODE
   if [[ "$GMOD_GAMEMODE" == "darkrp" ]] ; then
-  wget -O
-  echo ...
+  echo "Downloading DarkRP"
+  wget -O /home/$GAMETOINSTAL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/master.zip https://github.com/FPtje/DarkRP/archive/master.zip
+  echo "DarkRP Downloading success"
   fi
   if [[ "$GMOD_GAMEMODE" == "murder" ]] ; then
   
