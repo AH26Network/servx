96b6c2a732d1ec63509effd1ac67887d5f856388
AH26 <contact@ah26.ovh>
2016-08-18 23:36:28 +0200

    Update servx.sh

--- servx.sh
+++ servx.sh
@@ -57,14 +57,9 @@ apt-get install screen -y
 echo "$GAMETOINSTALL-$SRVID"
 read  -p "Enter password for $GAMETOINSTALL Server $SRVID : " password
 egrep "^$GAMETOINSTALL-$SRVID" /etc/passwd >/dev/null
-if [ $? -eq 0 ]; then
-	echo "$GAMETOINSTALL-$SRVID exists!"
-	exit 1
-else
-	pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-	useradd -m -p $pass $GAMETOINSTALL-$SRVID
-	[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-fi
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+useradd -m -p $pass $GAMETOINSTALL-$SRVID
+echo "User has been added to system!"
 
 echo "debug mode"
 
