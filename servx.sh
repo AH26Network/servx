4885e6c1d300074b09e84f2adef1a09eec650af0
AH26 <contact@ah26.ovh>
2016-08-19 02:03:58 +0200

    Update servx.sh

--- servx.sh
+++ servx.sh
@@ -108,11 +108,11 @@ fi
 if [[ "$GAMETOINSTALL" = "gmod" ]] ; then
 echo "Downloading SteamCMD ..."
 wget -O /home/$GAMETOINSTALL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-cd /home/$GAMETOINSTAL-$SRVID/
+cd /home/$GAMETOINSTALL-$SRVID/
 tar -xvzf steamcmd_linux.tar.gz
 echo "Steam Downloading success"
-chmod +x /home/$GAMETOINSTAL-$SRVID/steamcmd.sh
-chmod -R 777 /home/$GAMETOINSTAL-$SRVID/
+chmod +x /home/$GAMETOINSTALL-$SRVID/steamcmd.sh
+chmod -R 777 /home/$GAMETOINSTALL-$SRVID/
 echo "Downloading Gmod"
 ./steamcmd.sh +login anonymous +app_update 4020 validate +quit
 echo "Gmod Downloading success"
@@ -122,21 +122,21 @@ echo -e "Sandbox;DarkRP;Muder;PropHunt;JailBreak${NC}"
 read -p "What Game Mode" GMOD_GAMEMODE
 if [[ "$GMOD_GAMEMODE" == "darkrp" ]] ; then
 echo "Downloading DarkRP ..."
-wget -O /home/$GAMETOINSTAL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/darkrp.zip https://github.com/FPtje/DarkRP/archive/master.zip
-cd /home/$GAMETOINSTAL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/
+wget -O /home/$GAMETOINSTALL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/darkrp.zip https://github.com/FPtje/DarkRP/archive/master.zip
+cd /home/$GAMETOINSTALL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/
 unzip master.zip
 echo "DarkRP Downloading success"
 fi
 if [[ "$GMOD_GAMEMODE" == "prophunt" ]] ; then
 echo "Downloading PropHunt ..."
-wget -O /home/$GAMETOINSTAL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/prop_hunt.zip http://uk2.strategyinformer.com/v2/download/fcdef8af/garrysmod/prop_hunt.zip
+wget -O /home/$GAMETOINSTALL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/prop_hunt.zip http://uk2.strategyinformer.com/v2/download/fcdef8af/garrysmod/prop_hunt.zip
 cd /home/$GAMETOINSTAL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/
 unzip prop_hunt.zip
 echo "PropHunt Downloading Success"
 fi
 if [[ "$GMOD_GAMEMODE" == "murder" ]] ; then
 echo "Downloading Murder ..."
-wget -O /home/$GAMETOINSTAL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/murder.zip https://github.com/mechanicalmind/murder/archive/master.zip
+wget -O /home/$GAMETOINSTALL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/murder.zip https://github.com/mechanicalmind/murder/archive/master.zip
 cd /home/$GAMETOINSTAL-$SRVID/Steam/steamapps/common/GarrysModDS/garrysmod/gamemodes/
 unzip master.zip
 echo "Murder Downloading Success"  
