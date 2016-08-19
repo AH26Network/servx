3a878a258e686818779ce617ae82df8bd74736d8
AH26 <contact@ah26.ovh>
2016-08-19 12:39:59 +0200

    Update servx.sh

--- servx.sh
+++ servx.sh
@@ -154,8 +154,8 @@ tar -xzf steamcmd_linux.tar.gz
 echo "Downloading SteamCMD Success"
 chmod +x steamcmd.sh
 chmod -R 777 /home/$GAMETOINSTALL-$SRVID/
-su $GAMETOINSTALL-$SRVID;
-./steamcmd.sh +login anonymous +app_update 258550 validate +quit
+sudo -u $GAMETOINSTALL-$SRVID ./steamcmd.sh +login anonymous +app_update 258550 validate +quit
+#./steamcmd.sh +login anonymous +app_update 258550 validate +quit
 #sshpass -p $password ssh $GAMETOINSTALL-$SRVID@localhost './steamcmd.sh +login anonymous +app_update 258550 validate +quit'
 cd /home/$GAMETOINSTALL-$SRVID/Steam/steamapps/common/
 fi
