a1994272333ab4a5b0dbde1e69e8c8676943f185
OC018 <www.sebastienroulph@hotmail.fr>
2016-08-19 13:29:16 +0200

    Update Terraria.sh

--- dev/Terraria.sh
+++ dev/Terraria.sh
@@ -4,6 +4,7 @@ wget -O /home/$GAMETOINSTALL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.aka
 cd /home/$GAMETOINSTALL-$SRVID/
 tar -xzf steamcmd_linux.tar.gz
 echo "Downloading SteamCMD Success"
+echo "Downloading Terraria ... "
 chmod +x steamcmd.sh
 chmod -R 777 /home/$GAMETOINSTALL-$SRVID/
 sudo -u $GAMETOINSTALL-$SRVID ./steamcmd.sh +login anonymous +app_update 105600 validate +quit
