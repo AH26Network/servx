c56b02bc75e4c84a156423eab59e4d5ef7ff8903
AH26 <contact@ah26.ovh>
2016-08-19 02:14:22 +0200

    Update test.sh

--- dev/test.sh
+++ dev/test.sh
@@ -4,4 +4,4 @@ SRVID="1"
 password="1234"
 echo sshpass -p '$password' ssh $GAMETOINSTALL-$SRVID@localhost mkdir test
 sleep 1
-sshpass -p '$password' ssh $GAMETOINSTALL-$SRVID@localhost mkdir test
+sshpass -p $password ssh $GAMETOINSTALL-$SRVID@localhost 'mkdir test'
