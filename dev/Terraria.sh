512371427f480b04ea590cfd874f2216d268d87f
OC018 <www.sebastienroulph@hotmail.fr>
2016-08-19 13:25:13 +0200

    Update Terraria.sh

--- dev/Terraria.sh
+++ dev/Terraria.sh
@@ -1,2 +1,11 @@
 if [[ "$GAMETOINSTALL" = "terraria" ]] ; then
 echo "Downloading SteamCMD ..."
+wget -O /home/$GAMETOINSTALL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+cd /home/$GAMETOINSTALL-$SRVID/
+tar -xzf steamcmd_linux.tar.gz
+echo "Downloading SteamCMD Success"
+chmod +x steamcmd.sh
+chmod -R 777 /home/$GAMETOINSTALL-$SRVID/
+sudo -u $GAMETOINSTALL-$SRVID ./steamcmd.sh +login anonymous +app_update 105600 validate +quit
+echo "Gmod Downloading success"
+echo "screen -h 1024 -dmS mono --server --gc=sgen -O=all TerrariaServer.exe
