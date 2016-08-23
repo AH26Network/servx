5e22306d72499f2bbb64523e8d5f67562a3cbea6
AH26 <contact@ah26.ovh>
2016-08-23 13:58:31 +0200

    Update servx.sh

--- servx.sh
+++ servx.sh
@@ -290,18 +290,18 @@ echo "screen -h 1024 -dmS $GAMETOINSTALL-$SRVID ./RustDedicated -batchmode +serv
 fi
 
 # - Terraria - 
-#echo "Downloading SteamCMD ..."
-#wget -O /home/$GAMETOINSTALL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-#cd /home/$GAMETOINSTALL-$SRVID/
-#tar -xzf steamcmd_linux.tar.gz
-#echo "Downloading SteamCMD Success"
-#echo "Downloading Terraria ... "
-#chmod +x steamcmd.sh
-#chmod -R 777 /home/$GAMETOINSTALL-$SRVID/
-#sudo -u $GAMETOINSTALL-$SRVID ./steamcmd.sh +login anonymous +app_update 105600 validate +quit
-#echo "Terraria Downloading success"
-#echo "screen -h 1024 -dmS $GAMETOINSTALL-$SRVID mono --server --gc=sgen -O=all TerrariaServer.exe" >> /home/$GAMETOINSTALL-$SRVID/Steam/steamapps/common/Terraria/start.sh
-#fi
+echo "Downloading SteamCMD ..."
+wget -O /home/$GAMETOINSTALL-$SRVID/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+cd /home/$GAMETOINSTALL-$SRVID/
+tar -xzf steamcmd_linux.tar.gz
+echo "Downloading SteamCMD Success"
+echo "Downloading Terraria ... "
+chmod +x steamcmd.sh
+chmod -R 777 /home/$GAMETOINSTALL-$SRVID/
+sudo -u $GAMETOINSTALL-$SRVID ./steamcmd.sh +login anonymous +app_update 105600 validate +quit
+echo "Terraria Downloading success"
+echo "screen -h 1024 -dmS $GAMETOINSTALL-$SRVID mono --server --gc=sgen -O=all TerrariaServer.exe" >> /home/$GAMETOINSTALL-$SRVID/Steam/steamapps/common/Terraria/start.sh
+fi
 
 # - Dontstarve
 if [[ "$GAMETOINSTALL" = "dontstarve" ]] ; then
